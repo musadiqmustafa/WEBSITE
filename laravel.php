d39b57c19ce8d7ba78280eec0625aa97a28b93b5
Musadiq Mustafa <78734165+musadiqmustafa@users.noreply.github.com>
2023-03-11 22:50:12 +0500

    added laravel page

---
+++ laravel.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+
+    <meta name="google-site-verification" content="A3g5ZyDkaxOsT4AB5OZJN4ygv8G3FnTza0tYVRdMMJQ" />
+
+    <meta charset="utf-8">
+    <title>Laravel Development Company</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Arpatech gives expert-level Laravel development services to create super web applications with robust features and delivers a superlative level of Laravel Web services. ">
+    <meta name="author" content="">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <!--<link href="css/agency.min.css" rel="stylesheet">-->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/swiper.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+
+    <head>
+        <?php
+        include 'include/header-script.php';
+        ?>
+      
+    </head>
+    <style>
+        .swiper-container {
+            padding: 60px 0;
+        }
+
+        .drupal-side-form.wordpress-side-form {
+            background: #411855;
+            border: 10px solid rgba(165, 65, 61, 0.95);
+        }
+
+        .drupal-side-form.wordpress-side-form .form-control {
+            background-color: #411855;
+            border-bottom: 1px solid #ad3339;
+        }
+
+        .container-fluid.client-test.client-test-all {
+            background: #411855;
+        }
+
+        h2.wordpress-heading-dev {
+            display: inline-block;
+            border-bottom: 3px solid#411855;
+            padding-bottom: 12px;
+        }
+
+        a.wordpress-started {
+            color: #411855;
+            border: 2px solid#411855;
+        }
+
+        .need-ecommerce-main.outsource-wordpress {
+            background: #fff;
+        }
+
+        .need-ecommerce-left h2 {
+            font-family: 'montserratmedium';
+            font-weight: normal;
+        }
+
+        .creation-main-all h2 {
+            border-bottom: 3px solid#411855;
+            color: #4a4a4a;
+        }
+
+        .wordpress-box:hover {
+            background: #411855;
+            transition: all ease-in 0.2s;
+            color: #fff;
+
+
+
+        }
+
+        a.page-scroll.wordpress-btn-top {
+            color: #411855;
+        }
+    </style>
+
+<body>
+
+    <div class="preloader">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="spinner"></div>
+            </div>
+        </div>
+    </div>
+    <?php include 'include/header-allsocial.php'; ?>
+    <?php include 'include/all-menu.php'; ?>
+    <div class="search-overlay">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="search-layer"></div>
+                <div class="search-layer"></div>
+                <div class="search-layer"></div>
+                <div class="search-close">
+                    <span class="search-close-line"></span>
+                    <span class="search-close-line"></span>
+                </div>
+                <div class="search-form">
+                    <form>
+                        <input type="text" class="input-search" placeholder="Search here...">
+                        <button type="submit"><i class='bx bx-search'></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="inner-banner">
+        <div class="container">
+            <div class="inner-title text-center">
+                <h3>Services</h3>
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <i class='bx bx-chevrons-right'></i>
+                    </li>
+                    <li>Laravel
+                </ul>
+                </li>
+                </ul>
+            </div>
+        </div>
+        <div class="inner-shape">
+            <img src="assets/images/shape/inner-shape.png" alt="Images">
+        </div>
+    </div>
+
+
+    <div class="main-clor" id="second-sec">
+        <div class="container">
+            <div class="row">
+                <div class="choose-us-main">
+                    <h2 class="wordpress-heading-dev">Laravel - A php web framework</h2>
+                    <!-- <h5>And Let us worry about the rest! </h5> -->
+                    <div class="drupal-dev-main">
+                        <div class="col-md-12 col-lg-6 animated-steps animated" data-class="slideInLeft">
+                            <img class="img-responsive" src="img/cms/laravel1-big.png" alt="laravel Development flat line graphic">
+                        </div>
+                        <div class="col-md-12 col-lg-6 animated-steps animated" data-class="slideInRight">
+                            <p class="dev-text1">Laravel Framework is an open-source framework of Hypertext Preprocessor based on Model View Control (MVC) system. Laravel Framework development company offers architecture, components and equipment for builders to expand complex net packages quicker. The professional Laravel Framework developers tend to hold programs and maintain healthy applications.
+                                Laravel is such an internet application framework with easy-to-read, organized syntax. We trust development must be an exciting, and innovative experience to be actually pleasant. The highest advantage with Laravel is that it eliminates the element of complexity out of web development. It eases unusual responsibilities that people working on online projects undergo. This comprises of issues based on authentication, caching, routing, and sessions durations.
+                            </p>
+                           
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid creation-main creation-main-all text-center">
+        <div class="container">
+            <div class="row">
+                <h2>Laravel Maintenance & Support</h2>
+                <p class="wordpress-tagline">Our Laravel development company provides constant support & maintenance services to main the effectiveness of your web solutions.</p>
+                <div class="support-main-dev">
+                    <div class="col-sm-12 col-md-12 col-lg-4 text-center">
+                        <div class="support-dev">
+                            <img src="img/cms/laravel1-backup.png" alt="Website Backup icon">
+                            <h3>Website Backup</h3>
+                            <p>Backup your files and databases of Laravel application to Laravel's native cloud file system. Your data will be fully saved and you will be notified if something goes wrong with your backup.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-4 text-center">
+                        <div class="support-dev">
+                            <img src="img/cms/laravel1-security.png" alt="Security Monitoring icon">
+                            <h3>Security Monitoring</h3>
+                            <p>Our Laravel developers track and detect Laravel performance problems in real time and ensures our 24/7 availability to meet the SLA to ensures your Laravel app performs well.</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-4 text-center">
+                        <div class="support-dev">
+                            <img src="img/cms/laravel1-speed.png" alt="Professional support icon">
+                            <h3>Professional Help</h3>
+                            <p>Get help from our Laravel experts to provide you tailored solutions in the most competitive rate and to give you guidance in throughout your Laravel journey.</p>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="col-md-12 text-center">
+                
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="need-ecommerce-main outsource-wordpress container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-lg-5 animated-steps animated" data-class="slideInLeft">
+                    <div class="need-ecommerce-left wordpress-ecom-left">
+                        <h2>Laravel software development</h2>
+                        <p>With the usage of our Agile set of technological information, we work in clearly defined stages. We employ versatile technique, and offer complete solutions considering all phases of development, testing and deployment. Our experience in presenting custom designed Laravel programs within the stipulated time without compromising with the pleasant and effectiveness make us one of the sought-after
+                            We have the cumulative skillset to conceptualize your idea and implement it despite its complexity and challenges.
+                        </p>
+                      
+                    </div>
+                </div>
+                <div class="col-md-12 col-lg-7 animated-steps animated" data-class="slideInRight">
+                    <div class="need-ecommerce-right">
+                        <img class="laravel-software img-responsive" src="img/cms/laravel1-outsource.jpg" alt="developer team flat line graphic">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <div class="container-fluid wordpress-service">
+        <div class="container">
+            <div class="row text-center">
+                <h2 class="wordpress-heading-dev">Laravel Development Services</h2>
+                <p class="wordpress-tagline">What's the complete package in Laravel development services? Laravel employs an easy and clean syntax with MVC web software development. Laravel in comparison to other frameworks, is convenient to apply and offer simpler opportunities for development. Arpatech uses Laravel to expand, revamp and redesign websites and internet packages by leveraging its clean coding techniques and modular programming. Our group of skilled Laravel developers work with complicated applications with modules to develop mighty web applications with the aid of this platform to create an incredible output.
+                    A Laravel service container happens to be the immaculate tool that assigns and manages class dependencies and hold various components that are needed throughout the length of the application.
+                    The service container is required to inject the components available in the framework. With the presence of a service container, there is no need to manually inject the components, as a result tight coupling of code is prevented.
+                </p>
+                <br></br>
+
+                <div class="wordpress-service-main col-md-10 col-md-offset-1">
+                    <div class="col-md-12 col-sm-12 col-lg-4">
+                        <div class="wordpress-box">
+                            <img class="contrast" src="img/cms/laravel1-service.png" alt="laravel greyscale icon">
+                            <div style='font-size:large;  text-decoration: underline;'>Laravel Web Application</div>
+                            Laravel web application framework is expressive and elegant, offers an enjoyable and a creative experience to the users.
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-lg-4">
+                        <div class="wordpress-box">
+                            <img src="img/cms/cms.png" alt="Laravel CMS Development icon">
+                            <div style='font-size:large;  text-decoration: underline;'>Laravel CMS Development</div>
+                            Build a functional CMS with Laravel PHP framework and develop top-notch and high powered Laravel web applications.
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-lg-4">
+                        <div class="wordpress-box">
+                            <img src="img/cms/cart (2).png" alt="Laravel Ecommerce Solution icon">
+                            <div style='font-size:large;  text-decoration: underline;'>Laravel Ecommerce Solution</div>
+                            Make engaging and productive e-commerce applications with the Laravel development services. It contains everything to create your Laravel based online shop.
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-12 col-md-12">
+                        <div class="wordpress-box">
+                            <img src="img/cms/integration.png" alt="3rd party app Integration icon">
+                            <div style='font-size:large;  text-decoration: underline;'>3rd party app Integration</div>
+                            Here our expert Laravel developers are integrating Laravel development Services with third party APIs.
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-lg-4">
+                        <div class="wordpress-box">
+                            <img src="img/cms/extension.png" alt="Laravel Extension icon">
+                            <div style='font-size:large;  text-decoration: underline;'>Laravel Extension</div>
+                            We build innovative applications by using confirmed methodologies and superior internet improvement techniques after completing the process of bilateral discussion.
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-12 col-lg-4">
+                        <div class="wordpress-box">
+                            <img src="img/cms/migrate.png" alt="Laravel Migration Solution icon">
+                            <div style='font-size:large;  text-decoration: underline;'>Laravel Migration Solution</div>
+                            Now it is easy to migrate between Laravel platforms with the help of Laravel Migration Solutions offered by our skilled Laravel developers.
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <!-- <div class="container-fluid best-solution-main">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="best-solution-left">
+                                    <h3>Drupal Services:</h3>
+                                    <p>Our expert Drupal developers executes the client's idea to design, develop and deploy a custom Drupal website. We offer highly advanced and flexible Drupal web and module development services to meet your business goals.</p>
+                                    <h4>Drupal Module Development:</h4>
+                                    <p>Drupal module development meets the need of almost every website with the availability of thousands of Drupal add-on modules. Module development with Drupal will helps you administer your website, supports events, builds community and easily manage media files.</p>
+                                    <h4>Website Development Services:</h4>
+                                    <p>Our Drupal website developers works out comprehensive plans and frameworks using diverse technologies which suits perfectly for the development needs of a mobile website. We use Drupal standard framework which is highly scalable and easily upgradable for Drupal applications.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div class="best-solution-right">
+                                    <img class="img-responsive" src="img/team/ecom1.gif" alt="Drupal Development ">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+
+
+
+
+
+    <?php include 'include/footer.php'; ?>
+
+
+
+    <script src="assets/js/jquery.min.js"></script>
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+
+    <script src="assets/js/wow.min.js"></script>
+
+    <script src="assets/js/meanmenu.js"></script>
+
+    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <script src="assets/js/form-validator.min.js"></script>
+
+    <script src="assets/js/contact-form-script.js"></script>
+
+    <script src="assets/js/custom.js"></script>
+
+    <script src="assets/js/functions.js"></script>
+</body>
+
+</html>
+<script>
+    
+</script>
